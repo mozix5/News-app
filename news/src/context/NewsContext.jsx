f050019b4bcf5e167fe63e3f69203f8cd3a381d2
mozix5 <76263118+mozix5@users.noreply.github.com>
2023-08-21 18:19:29 +0530

    Update NewsContext.jsx

--- news/src/context/NewsContext.jsx
+++ news/src/context/NewsContext.jsx
@@ -21,6 +21,7 @@ export const useSearchQuery = () => {
 
 export const NewsContext = ({ children }) => {
   const [data, setData] = useState([]);
+  const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [modalContent, setModalContent] = useState({});
   const [query, setQuery] = useState("");
@@ -31,14 +32,31 @@ export const NewsContext = ({ children }) => {
       .get("https://newsdata.io/api/1/news", {
         params: {
           apikey: "pub_20018d3a2caa7e2ecc50ace9557a2a820c07d",
-          q: query,
+          q: query === "" ? "top" : query,
           country: "us",
         },
       })
       .then((res) => {
         setData(res.data.results);
+        // console.log(data);
         setIsLoading(false);
       });
+      axios.get('https://pixabay.com/api/', {
+      params: {
+        key: '38965543-d1b30d42f779a8cd8c0bd4480',
+        q: query,
+        image_type: 'photo',
+        min_width:"320px"
+      }
+    }).then(response => {
+      setImages(response.data.hits);
+      // setIsLoading(false);
+    // console.log(images[4].webformatURL);
+
+    }).catch(error => {
+      console.error(error);
+      setIsLoading(false);
+    });
   };
 
   useEffect(() => {
@@ -56,7 +74,7 @@ export const NewsContext = ({ children }) => {
 
   return (
     <LoadingContext.Provider value={{ isLoading, setIsLoading }}>
-      <DataContext.Provider value={{ data }}>
+      <DataContext.Provider value={{ data,images }}>
         <ModalContext.Provider value={{ modalContent, setModalContent }}>
           <SearchContext.Provider value={{ query, handleChange,handleSubmit }}>
             {children}
